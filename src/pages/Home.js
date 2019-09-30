349d1341a6ef49991bce10eca2a327ce4aa23951
Fardhan Ardhi <fa.dsg.id@gmail.com>
2019-09-30 10:46:20 +0700

    add router

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-export default class Login extends Component {
+export default class Home extends Component {
   render() {
     return (
       <div>
         <nav>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/login">Login</Link>
             </li>
           </ul>
         </nav>
-        <h2>Login</h2>
+        <h2>Home</h2>
       </div>
     );
   }
---
+++ src/routes/Routes.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import Home from "../pages/Home";
+
+export default function Routes() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
