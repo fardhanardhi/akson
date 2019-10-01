d10199d0e70d91133a10b425af74960796b1bdef
SulthanRafif <rsulthan5@gmail.com>
2019-10-01 10:56:59 +0700

    Pause button almost done

--- src/components/PauseMenu.js
+++ src/components/PauseMenu.js
@@ -1,49 +1,63 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Play from "../assets/play.svg";
+import Restart from "../assets/restart.svg";
+import X from "../assets/x.svg";
 
 export default class PauseMenu extends Component {
-    render() {
-        return (
-            <div>
-                <div>
-                    <div style={style.shape}>
-                        <a style={style.tulisanSatu}>Mencocokkan objek</a>
-                        <a>Temukan objek yang berpasangan</a>
-                    </div>
+  render() {
+    return (
+      <div>
+        <div style={style.shape}>
+          <div className="mt-4 ml-4" style={style.tulisanSatu}>
+            Mencocokkan objek
+          </div>
+          <div className="ml-5" style={style.tulisanDua}>
+            Temukan objek yang berpasangan
+          </div>
+          <div className="mt-4 mr-5">
+            <div className="container">
+              <div className="col-md-12">
+                <div className="row">
+                  <div className="col-md-4">
+                    <image src={Play}></image>
+                  </div>
+                  <div className="col-md-4">
+                    <image src={Restart}></image>
+                  </div>
+                  <div className="col-md-4">
+                    <image src={X}></image>
+                  </div>
                 </div>
+              </div>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 
 const style = {
-    shape: {
-        width: "511px",
-        height: "283px",
-        left: "432px",
-        top: "201px",
+  shape: {
+    width: "511px",
+    height: "283px",
 
-        background: "#EFFFF0",
-        borderRadius: "40px"
-    },
+    background: "#EFFFF0",
+    borderRadius: "40px",
 
-    tulisanSatu: {
-        // position: "absolute",
-        width: "342px",
-        height: "73px",
-        left: "515px",
-        top: "232px",
+    padding: "5px",
+    paddingLeft: "60px"
+  },
 
-        fontFamily: "Carter One",
-        fontStyle: "normal",
-        fontWeight: "normal",
-        fontSize: "35px",
-        lineHeight: "54px",
-        textAlign: "center",
-        
-        color: "#000000"
+  tulisanSatu: {
+    fontFamily: "Carter One",
+    fontSize: "35px",
 
-    }
-   
-   
-}
+    color: "#000000"
+  },
+
+  tulisanDua: {
+    fontFamily: "Roboto",
+    fontSize: "18px"
+  }
+};
