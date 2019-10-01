c55ecbf5a45283ca284189b24264879a4ce0de88
SulthanRafif <rsulthan5@gmail.com>
2019-10-01 12:11:29 +0700

    menyelesaikan menu pause permainan

--- src/pages/Permainan.js
+++ src/pages/Permainan.js
@@ -4,9 +4,16 @@ import PauseMenu from "../components/PauseMenu";
 export default class Permainan extends Component {
     render() {
         return (
-           <div>
+           <div className="d-flex justify-content-center" style={styles}>
                <PauseMenu/>
            </div>    
         )
     }
+}
+
+const styles={
+    // minHeight: '100%', 
+    minHeight: '100vh',
+    display: 'flex',
+    alignItems: 'center',
 }
