4f3b4c31ce47f6361b31248ee6b70266775fa3ed
navalinovian <viansatria57@gmail.com>
2019-10-01 11:27:57 +0700

    homePage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PlayButton from "../assets/playButton.svg";
+import LogoComplete from "../assets/logoComplete.svg";
+import Background from "../assets/Background.svg";
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
+      <div style={style.bgStyle}>
         <nav>
           <ul>
             <li>
@@ -12,8 +15,51 @@ export default class Home extends Component {
             </li>
           </ul>
         </nav>
-        <h2>Home</h2>
+        <div className="container-fluid ">
+          <div style={style.nganu} className="row" >
+            <div className="col-md-12">
+              <div className="row">
+                <img
+                  src={LogoComplete}
+                  alt="logoComplete"
+                  style={style.logoComStyle}
+                ></img>
+              </div>
+              <div class="row">
+                <img
+                  src={PlayButton}
+                  alt="playbtn"
+                  style={style.playBtnStyle}
+                ></img>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+const style = {
+  bgStyle: {
+    backgroundPosition: "center center",
+    backgroundRepeat: "no-repeat",
+    backgroundImage: `url(${Background})`,
+    height: "100vh"
+  },
+  logoComStyle: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    padding: "auto",
+    // margin: "auto",
+    zIndex: "1"
+  },
+
+  playBtnStyle: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    padding: "auto",
+    // margin: "auto",
+    zIndex: "2"
+  }
+};
