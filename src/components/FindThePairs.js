57875845259c314361813b52361312a5e103408d
Fardhan Ardhi <fa.dsg.id@gmail.com>
2019-10-14 21:14:41 +0700

    fix find the pairs choices

--- src/components/FindThePairs.js
+++ src/components/FindThePairs.js
@@ -34,14 +34,7 @@ export default class FindThePairs extends Component {
   }
 
   componentDidMount() {
-    const choices = [...Array(12)].map(x => {
-      return {
-        object: this.getRandomId(this.objects),
-        color: this.getRandomId(colors)
-      };
-    });
-
-    this.setState({ choices }, () => console.log(this.state.choices));
+    this.getChoices();
   }
 
   getRandomId = arr => {
@@ -64,6 +57,103 @@ export default class FindThePairs extends Component {
     return obj[objectId];
   };
 
+  getChoices = () => {
+    // generate pilihan jawaban benar
+    let rightChoices = [...Array(3)].map(x => {
+      return {
+        object: this.getRandomId(this.objects),
+        color: this.getRandomId(colors)
+      };
+    });
+
+    // cek pilihan jawaban benar
+    let rightChoicesNotPassed = true;
+    while (rightChoicesNotPassed) {
+      for (let i = 0; i < rightChoices.length; i++) {
+        for (let j = 0; j < rightChoices.length; j++) {
+          if (i !== j) {
+            if (
+              rightChoices[i].object === rightChoices[j].object &&
+              rightChoices[i].color === rightChoices[j].color
+            ) {
+              rightChoices = rightChoices.map(x => {
+                return {
+                  object: this.getRandomId(this.objects),
+                  color: this.getRandomId(colors)
+                };
+              });
+            } else {
+              rightChoicesNotPassed = false;
+            }
+          }
+        }
+      }
+    }
+
+    // duplikasi pilihan jawaban benar
+    rightChoices = [...rightChoices, ...rightChoices];
+
+    // generate pilihan jawaban salah
+    let wrongChoices = [...Array(6)].map(x => {
+      return {
+        object: this.getRandomId(this.objects),
+        color: this.getRandomId(colors)
+      };
+    });
+
+    // cek pilihan jawaban benar
+    let wrongChoicesNotPassed1 = true;
+    let wrongChoicesNotPassed2 = true;
+    while (wrongChoicesNotPassed1 || wrongChoicesNotPassed2) {
+      for (let i = 0; i < rightChoices.length; i++) {
+        for (let j = 0; j < rightChoices.length; j++) {
+          if (i !== j) {
+            if (
+              rightChoices[i].object === wrongChoices[j].object &&
+              rightChoices[i].color === wrongChoices[j].color
+            ) {
+              wrongChoices = wrongChoices.map(x => {
+                return {
+                  object: this.getRandomId(this.objects),
+                  color: this.getRandomId(colors)
+                };
+              });
+            } else {
+              wrongChoicesNotPassed1 = false;
+            }
+          }
+        }
+      }
+      for (let i = 0; i < wrongChoices.length; i++) {
+        for (let j = 0; j < wrongChoices.length; j++) {
+          if (i !== j) {
+            if (
+              wrongChoices[i].object === wrongChoices[j].object &&
+              wrongChoices[i].color === wrongChoices[j].color
+            ) {
+              wrongChoices = wrongChoices.map(x => {
+                return {
+                  object: this.getRandomId(this.objects),
+                  color: this.getRandomId(colors)
+                };
+              });
+            } else {
+              wrongChoicesNotPassed2 = false;
+            }
+          }
+        }
+      }
+    }
+
+    // penggabungan pilihan jawaban salah & benar
+    let hasil = [...rightChoices, ...wrongChoices];
+
+    // acak posisi pilihan jawaban
+    const choices = [...hasil].sort(() => Math.random() - 0.5);
+
+    this.setState({ choices });
+  };
+
   render() {
     return (
       <div className="container text-center m-0 p-0">
@@ -72,7 +162,7 @@ export default class FindThePairs extends Component {
             let component = null;
             if (index < 4) {
               component = (
-                <div className="col-md-3">
+                <div className="col-md-3" key={index}>
                   {this.getObject(item.object, item.color)}
                 </div>
               );
@@ -87,15 +177,14 @@ export default class FindThePairs extends Component {
             let component = null;
 
             if (index < 8 && index >= 4) {
-              
-                component = (<div className="col-md-3">
-                {this.getObject(item.object, item.color)}
-              </div>
+              component = (
+                <div className="col-md-3" key={index}>
+                  {this.getObject(item.object, item.color)}
+                </div>
               );
-            }
-            else{
+            } else {
               component = null;
-            }  
+            }
             return component;
           })}
         </div>
@@ -103,13 +192,12 @@ export default class FindThePairs extends Component {
           {this.state.choices.map((item, index) => {
             let component = null;
             if (index < 12 && index >= 8) {
-              component =
-                (<div className="col-md-3">
+              component = (
+                <div className="col-md-3" key={index}>
                   {this.getObject(item.object, item.color)}
                 </div>
               );
-            }
-            else{
+            } else {
               component = null;
             }
             return component;
