83521d7ee4ddf02e7fc33a04c2e18928db213fb3
SulthanRafif <rsulthan5@gmail.com>
2019-10-28 08:15:55 +0700

    push

--- src/components/ShapeAndPattern.js
+++ src/components/ShapeAndPattern.js
@@ -80,7 +80,24 @@ export default class ShapeAndPattern extends Component {
           d="M70.5 0L87.0018 50.7873H140.403L97.2004 82.1755L113.702 132.963L70.5 101.575L27.2978 132.963L43.7996 82.1755L0.597343 50.7873H53.9982L70.5 0Z"
           style={{ fill: pattern[randProps].url() }}
         />
-      </svg>
+      </svg>,
+
+      <svg 
+        width="131" 
+        height="131" 
+        viewBox="0 0 131 131" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg">
+        <circle
+          cx="65.5" 
+          cy="65.5" 
+          r="65.5"
+          style={{fill: pattern[randProps].url()}}
+        />
+        <defs
+           dangerouslySetInnerHTML={{ __html: stringify(pattern[randProps]) }}
+        />
+      </svg> 
     ];
     return obj[objectId];
   };
