1ee36131b75bb616dcfc470ea5eb94fe837a9941
SulthanRafif <rsulthan5@gmail.com>
2019-11-03 12:08:22 +0700

    shape and pattern yuhu

---
+++ src/components/Pattern.js
@@ -0,0 +1,109 @@
+import patterns from "svg-patterns"
+
+const pattern = [
+    patterns.lines({
+        stroke: "#000000",
+        background: "#2196f3",
+        name: "lines blue"
+      }),
+      patterns.lines({
+        stroke: "#000000",
+        background: "#9e9e9e",
+        name: "lines grey"
+      }),
+      patterns.lines({
+        stroke: "#000000",
+        background: "#4caf50",
+        name: "lines green"
+      }),
+      patterns.lines({
+        stroke: "#000000",
+        background: "#ff9800",
+        name: "lines orange"
+      }),
+      patterns.lines({
+        stroke: "#000000",
+        background: "#f44336",
+        name: "lines red"
+      }),
+
+      patterns.caps({
+        stroke: "#000000",
+        background: "#2196f3",
+        name: "caps blue"
+      }),
+      patterns.caps({
+        stroke: "#000000",
+        background: "#9e9e9e",
+        name: "caps grey"
+      }),
+      patterns.caps({
+        stroke: "#000000",
+        background: "#4caf50",
+        name: "caps green"
+      }),
+      patterns.caps({
+        stroke: "#000000",
+        background: "#ff9800",
+        name: "caps orange"
+      }),
+      patterns.caps({
+        stroke: "#000000",
+        background: "#f44336",
+        name: "caps red"
+      }),
+
+      patterns.hexagons({
+        stroke: "#000000",
+        background: "#2196f3",
+        name: "hexagons blue"
+      }),
+      patterns.hexagons({
+        stroke: "#000000",
+        background: "#9e9e9e",
+        name: "hexagons grey"
+      }),
+      patterns.hexagons({
+        stroke: "#000000",
+        background: "#4caf50",
+        name: "hexagons green"
+      }),
+      patterns.hexagons({
+        stroke: "#000000",
+        background: "#ff9800",
+        name: "hexagons orange"
+      }),
+      patterns.hexagons({
+        stroke: "#000000",
+        background: "#f44336",
+        name: "hexagons red"
+      }),
+
+      patterns.circles({
+        stroke: "#000000",
+        background: "#2196f3",
+        name: "circle blue"
+      }),
+      patterns.circles({
+        stroke: "#000000",
+        background: "#9e9e9e",
+        name: "circle grey"
+      }),
+      patterns.circles({
+        stroke: "#000000",
+        background: "#4caf50",
+        name: "circle green"
+      }),
+      patterns.circles({
+        stroke: "#000000",
+        background: "#ff9800",
+        name: "circle orange"
+      }),
+      patterns.circles({
+        stroke: "#000000",
+        background: "#f44336",
+        name: "circle red"
+      })
+];
+
+export default pattern;
--- src/components/ShapeAndPattern.js
+++ src/components/ShapeAndPattern.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
-import patterns from "svg-patterns";
-import stringify from "virtual-dom-stringify";
 
-import Circle from "../components/Circle";
+import Star from "../components/Stars";
 import Rectangel from "../components/Rectangel";
-import Stars from "../components/Stars";
-import Rectangular from "../components/Rectangular";
+import Circle from "../components/Circle";
+import Rectangular from "../components/Rectangel";
 import Parallelogram from "../components/Parallelogram";
 
-import colors from "../assets/colors";
-import GameHeader from "./GameHeader";
+import GameHeader from "../components/GameHeader";
 
-export default class ShapeAndPattern extends Component {
+import stringify from "virtual-dom-stringify";
+import Pattern from "../components/Pattern";
+
+export default class FindThePairs extends Component {
   constructor(props) {
     super(props);
 
@@ -22,7 +22,7 @@ export default class ShapeAndPattern extends Component {
     this.objects = [
       <Circle />,
       <Rectangel />,
-      <Stars />,
+      <Star />,
       <Rectangular />,
       <Parallelogram />
     ];
@@ -37,194 +37,121 @@ export default class ShapeAndPattern extends Component {
     return id;
   };
 
-  getObject = objectId => {
-    const pattern = [
-      patterns.lines({
-        stroke: "#000000",
-        background: "#2196f3"
-      }),
-      patterns.lines({
-        stroke: "#000000",
-        background: "#9e9e9e"
-      }),
-      patterns.lines({
-        stroke: "#000000",
-        background: "#4caf50"
-      }),
-      patterns.lines({
-        stroke: "#000000",
-        background: "#ff9800"
-      }),
-      patterns.lines({
-        stroke: "#000000",
-        background: "#f44336"
-      }),
-
-      patterns.caps({
-        stroke: "#000000",
-        background: "#2196f3"
-      }),
-      patterns.caps({
-        stroke: "#000000",
-        background: "#9e9e9e"
-      }),
-      patterns.caps({
-        stroke: "#000000",
-        background: "#4caf50"
-      }),
-      patterns.caps({
-        stroke: "#000000",
-        background: "#ff9800"
-      }),
-      patterns.caps({
-        stroke: "#000000",
-        background: "#f44336"
-      }),
-
-      patterns.hexagons({
-        stroke: "#000000",
-        background: "#2196f3"
-      }),
-      patterns.hexagons({
-        stroke: "#000000",
-        background: "#9e9e9e"
-      }),
-      patterns.hexagons({
-        stroke: "#000000",
-        background: "#4caf50"
-      }),
-      patterns.hexagons({
-        stroke: "#000000",
-        background: "#ff9800"
-      }),
-      patterns.hexagons({
-        stroke: "#000000",
-        background: "#f44336"
-      }),
-
-      patterns.circles({
-        stroke: "#000000",
-        background: "#2196f3"
-      }),
-      patterns.circles({
-        stroke: "#000000",
-        background: "#9e9e9e"
-      }),
-      patterns.circles({
-        stroke: "#000000",
-        background: "#4caf50"
-      }),
-      patterns.circles({
-        stroke: "#000000",
-        background: "#ff9800"
-      }),
-      patterns.circles({
-        stroke: "#000000",
-        background: "#f44336"
-      })
-    ];
-
-    const randProps = Math.floor(Math.random() * pattern.length);
-
+  getObject = (objectId, patternId, patternIdName) => {
     const obj = [
-      <svg
-        width="152"
-        height="135"
-        viewBox="0 0 152 135"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <defs
-          dangerouslySetInnerHTML={{ __html: stringify(pattern[randProps]) }}
-        />
+      <div>
+        <svg
+          width="152"
+          height="135"
+          viewBox="0 0 152 135"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs
+            dangerouslySetInnerHTML={{ __html: stringify(Pattern[patternId]) }}
+          />
 
-        <path
-          d="M76 0L151.344 134.25H0.655792L76 0Z"
-          style={{ fill: pattern[randProps].url() }}
-        />
-      </svg>,
+          <path
+            d="M76 0L151.344 134.25H0.655792L76 0Z"
+            style={{ fill: Pattern[patternId].url() }}
+          />
+        </svg>
+        <p style={styles.textHelp}>{`Segitiga + ${Pattern[patternIdName].name}`}</p>
+      </div>,
 
-      <svg
-        width="141"
-        height="133"
-        viewBox="0 0 141 133"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <defs
-          dangerouslySetInnerHTML={{ __html: stringify(pattern[randProps]) }}
-        />
-        <path
-          d="M70.5 0L87.0018 50.7873H140.403L97.2004 82.1755L113.702 132.963L70.5 101.575L27.2978 132.963L43.7996 82.1755L0.597343 50.7873H53.9982L70.5 0Z"
-          style={{ fill: pattern[randProps].url() }}
-        />
-      </svg>,
+      <div>
+        <svg
+          width="141"
+          height="133"
+          viewBox="0 0 141 133"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs
+            dangerouslySetInnerHTML={{ __html: stringify(Pattern[patternId]) }}
+          />
+          <path
+            d="M70.5 0L87.0018 50.7873H140.403L97.2004 82.1755L113.702 132.963L70.5 101.575L27.2978 132.963L43.7996 82.1755L0.597343 50.7873H53.9982L70.5 0Z"
+            style={{ fill: Pattern[patternId].url() }}
+          />
+        </svg>
+        <p style={styles.textHelp}>Stars</p>
+      </div>,
 
-      <svg
-        width="131"
-        height="131"
-        viewBox="0 0 131 131"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <circle
-          cx="65.5"
-          cy="65.5"
-          r="65.5"
-          style={{ fill: pattern[randProps].url() }}
-        />
-        <defs
-          dangerouslySetInnerHTML={{ __html: stringify(pattern[randProps]) }}
-        />
-      </svg>,
+      <div>
+        <svg
+          width="131"
+          height="131"
+          viewBox="0 0 131 131"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle
+            cx="65.5"
+            cy="65.5"
+            r="65.5"
+            style={{ fill: Pattern[patternId].url() }}
+          />
+          <defs
+            dangerouslySetInnerHTML={{ __html: stringify(Pattern[patternId]) }}
+          />
+        </svg>
+        <p style={styles.textHelp}>Circle</p>
+      </div>,
 
-      <svg
-        width="129"
-        height="129"
-        viewBox="0 0 129 129"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <rect
+      <div>
+        <svg
           width="129"
           height="129"
-          style={{ fill: pattern[randProps].url() }}
-        />
-        <defs
-          dangerouslySetInnerHTML={{ __html: stringify(pattern[randProps]) }}
-        />
-      </svg>,
-      <svg
-        width="270"
-        height="80"
-        viewBox="0 0 270 80"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M0 79.5L57.5 0H270L215.5 79.5H0Z"
-          style={{ fill: pattern[randProps].url() }}
-        />
-        <defs
-          dangerouslySetInnerHTML={{ __html: stringify(pattern[randProps]) }}
-        />
-      </svg>
+          viewBox="0 0 129 129"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            width="129"
+            height="129"
+            style={{ fill: Pattern[patternId].url() }}
+          />
+          <defs
+            dangerouslySetInnerHTML={{ __html: stringify(Pattern[patternId]) }}
+          />
+        </svg>
+        <p style={styles.textHelp}>Rectangular</p>
+      </div>,
+      <div>
+        <svg
+          width="270"
+          height="80"
+          viewBox="0 0 270 80"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0 79.5L57.5 0H270L215.5 79.5H0Z"
+            style={{ fill: Pattern[patternId].url() }}
+          />
+          <defs
+            dangerouslySetInnerHTML={{ __html: stringify(Pattern[patternId]) }}
+          />
+        </svg>
+        <p style={styles.textHelp}>Parallelogram</p>
+      </div>
     ];
     return obj[objectId];
   };
 
   getChoices = () => {
     // generate pilihan jawaban benar
-
     let rightChoices = [...Array(3)].map(x => {
       return {
         object: this.getRandomId(this.objects),
-        color: this.getRandomId(colors)
+        pattern: this.getRandomId(Pattern),
+        isSelected: false,
+        isCorrect: false
       };
     });
 
     // cek pilihan jawaban benar
-
     let rightChoicesNotPassed = true;
     while (rightChoicesNotPassed) {
       for (let i = 0; i < rightChoices.length; i++) {
@@ -232,12 +159,12 @@ export default class ShapeAndPattern extends Component {
           if (i !== j) {
             if (
               rightChoices[i].object === rightChoices[j].object &&
-              rightChoices[i].color === rightChoices[j].color
+              rightChoices[i].pattern === rightChoices[j].pattern
             ) {
               rightChoices = rightChoices.map(x => {
                 return {
                   object: this.getRandomId(this.objects),
-                  color: this.getRandomId(colors)
+                  pattern: this.getRandomId(Pattern)
                 };
               });
             } else {
@@ -249,32 +176,30 @@ export default class ShapeAndPattern extends Component {
     }
 
     // duplikasi pilihan jawaban benar
-
     rightChoices = [...rightChoices, ...rightChoices];
 
+    // generate pilihan jawaban salah
     let wrongChoices = [...Array(6)].map(x => {
       return {
         object: this.getRandomId(this.objects),
-        color: this.getRandomId(colors)
+        pattern: this.getRandomId(Pattern),
+        isSelected: false,
+        isCorrect: false
       };
     });
 
     // cek pilihan jawaban benar
-
     let wrongChoicesNotPassed1 = true;
     let wrongChoicesNotPassed2 = true;
     while (wrongChoicesNotPassed1 || wrongChoicesNotPassed2) {
       for (let i = 0; i < rightChoices.length; i++) {
         for (let j = 0; j < rightChoices.length; j++) {
           if (i !== j) {
-            if (
-              rightChoices[i].object === wrongChoices[j].object &&
-              rightChoices[i].color === wrongChoices[j].color
-            ) {
+            if (rightChoices[i].object === wrongChoices[j].object) {
               wrongChoices = wrongChoices.map(x => {
                 return {
                   object: this.getRandomId(this.objects),
-                  color: this.getRandomId(colors)
+                  pattern: this.getRandomId(Pattern)
                 };
               });
             } else {
@@ -283,18 +208,17 @@ export default class ShapeAndPattern extends Component {
           }
         }
       }
-
       for (let i = 0; i < wrongChoices.length; i++) {
         for (let j = 0; j < wrongChoices.length; j++) {
           if (i !== j) {
             if (
               wrongChoices[i].object === wrongChoices[j].object &&
-              wrongChoices[i].color === wrongChoices[j].color
+              wrongChoices[i].pattern === wrongChoices[j].pattern
             ) {
               wrongChoices = wrongChoices.map(x => {
                 return {
                   object: this.getRandomId(this.objects),
-                  color: this.getRandomId(colors)
+                  pattern: this.getRandomId(Pattern)
                 };
               });
             } else {
@@ -309,15 +233,24 @@ export default class ShapeAndPattern extends Component {
     let hasil = [...rightChoices, ...wrongChoices];
 
     // acak posisi pilihan jawaban
-
     const choices = [...hasil].sort(() => Math.random() - 0.5);
 
     this.setState({ choices });
   };
 
+  selectChoice = index => {
+    let choices = this.state.choices;
+    choices[index].isSelected = !choices[index].isSelected;
+    this.setState({ choices: choices });
+    console.log("cois: ", choices[index]);
+  };
+
   render() {
+    console.log("gameinfo ftp:", this.props.gameInfo);
+    console.log(this.state.choices);
+
     return (
-      <div className="container-fluid text-center m-0 p-0">
+      <div className="container-fluid p-0">
         {this.props.gameInfo == null ? null : (
           <GameHeader
             gameInfo={this.props.gameInfo}
@@ -327,52 +260,93 @@ export default class ShapeAndPattern extends Component {
             ref={this.headerRef}
           />
         )}
-        <h2 style={styles.text}>Pattern</h2>
-        <div className="row">
-          {this.state.choices.map((item, index) => {
-            let component = null;
-            if (index < 4) {
-              component = (
-                <div className="col-md-3" key={index}>
-                  {this.getObject(item.object, item.color)}
-                </div>
-              );
-            } else {
-              component = null;
-            }
+        <div className="container text-center m-0 p-0">
+          <h2 style={styles.text}>Shape and pattern</h2>
+          <div className="row">
+            {this.state.choices.map((item, index) => {
+              let component = null;
+              if (index < 4) {
+                component = (
+                  <div className="col-md-3" key={index}>
+                    <div className="row">
+                      <div className="col"></div>
+                      <div
+                        className={
+                          item.isSelected
+                            ? "col-auto aks-btn bg-success"
+                            : "col-auto aks-btn"
+                        }
+                        onClick={() => this.selectChoice(index)}
+                      >
+                        {this.getObject(item.object, item.pattern)}
+                      </div>
+                      <div className="col"></div>
+                    </div>
+                  </div>
+                );
+              } else {
+                component = null;
+              }
+              return component;
+            })}
+          </div>
+          <div className="row mt-5">
+            {this.state.choices.map((item, index) => {
+              let component = null;
 
-            return component;
-          })}
-        </div>
-        <div className="row mt-5">
-          {this.state.choices.map((item, index) => {
-            let component = null;
-            if (index < 8 && index >= 4) {
-              component = (
-                <div className="col-md-3" key={index}>
-                  {this.getObject(item.object, item.color)}
-                </div>
-              );
-            } else {
-              component = null;
-            }
-            return component;
-          })}
-        </div>
-        <div className="row mt-5">
-          {this.state.choices.map((item, index) => {
-            let component = null;
-            if (index < 12 && index >= 8) {
-              component = (
-                <div className="col-md-3" key={index}>
-                  {this.getObject(item.object, item.color)}
-                </div>
-              );
-            } else {
-              component = null;
-            }
-            return component;
-          })}
+              if (index < 8 && index >= 4) {
+                component = (
+                  <div className="col-md-3" key={index}>
+                    <div className="row">
+                      <div className="col"></div>
+                      <div
+                        className={
+                          item.isSelected
+                            ? "col-auto aks-btn bg-success"
+                            : "col-auto aks-btn"
+                        }
+                        onClick={() => this.selectChoice(index)}
+                      >
+                        {this.getObject(item.object, item.pattern)}
+                      </div>
+                      <div className="col"></div>
+                    </div>
+                  </div>
+                );
+              } else {
+                component = null;
+              }
+              return component;
+            })}
+          </div>
+          <div className="row mt-5">
+            {this.state.choices.map((item, index) => {
+              let component = null;
+              if (index < 12 && index >= 8) {
+                component = (
+                  <div className="col-md-3" key={index}>
+                    <div className="row">
+                      <div className="col"></div>
+                      <div
+                        className={
+                          item.isSelected
+                            ? "col-auto aks-btn bg-success"
+                            : "col-auto aks-btn"
+                        }
+                        onClick={() => this.selectChoice(index)}
+                      >
+                        {this.getObject(item.object, item.pattern)}
+                      </div>
+                      <div className="col"></div>
+                    </div>
+                  </div>
+                );
+              } else {
+                component = null;
+              }
+              return component;
+            })}
+          </div>
         </div>
       </div>
     );
@@ -385,5 +359,15 @@ const styles = {
     fontFamily: "Carter One",
     color: "#1D1D1D",
     marginBottom: "75px"
+  },
+
+  textHelp: {
+    position: "absolute",
+    fontFamily: "Carter One",
+    fontSize: "1.5vw",
+    color: "#1D1D1D",
+    top: "17px",
+    marginLeft: "105px",
+    cursor: "pointer"
   }
 };
