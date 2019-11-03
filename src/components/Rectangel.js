c2830196324b8172a4579337b8c63aa09a061f19
panjiad <panjiawwaludi430@gmail.com>
2019-11-03 18:19:33 +0700

    text in color codes

--- src/components/Rectangel.js
+++ src/components/Rectangel.js
@@ -2,14 +2,7 @@ import React from'react'
 
 export default function Rectangel(props){
     const styles = {
-        text:{
-            position : "absolute",
-            fontSize : "1.5vw",
-            color : "#FAFAFA",
-            top : "33px",
-            marginLeft : "69px",
-            cursor: "pointer"
-        },
+        
         btn:{
             cursor: "pointer"
         }
@@ -17,10 +10,11 @@ export default function Rectangel(props){
     
     return(
         <div onClick={props.onClick}>  
-            <svg width="138" height="70" viewBox="0 0 138 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect width="138" height="70" fill={props.color} style={styles.btn} />
+            <svg width="138" height="70" viewBox="0 0 138 70"  style={styles.btn} fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="138" height="70" fill={props.color} />
+                <text textAnchor="middle" x="50%" y="55%" fill="#F0F0F0">{props.name}</text>
             </svg>
-            <p style={styles.text}>{props.name}</p>
+            {/* <p style={styles.text}>{props.name}</p> */}
         </div>
     )
 }
