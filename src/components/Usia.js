b094c0de52448f87058912e473dfa0da5e776666
SulthanRafif <rsulthan5@gmail.com>
2019-12-09 08:32:22 +0700

    fix usia

--- src/components/Usia.js
+++ src/components/Usia.js
@@ -96,25 +96,26 @@ export default class Usia extends Component {
               }}
             />
           </div>
-          <div className="container">
+          <div className="container mt-3">
             <div className="col-md-12">
               <div className="row">
                 <div className="col"></div>
-                <Link to ="/menu">
+                <Link to="/menu">
                   <div
                     className="col-auto ml-3 mt-5 aks-btn"
-                    style={style.kotakLain}
                   >
-                    <div className="container">
-                      <div className="col-md-12">
-                        <div className="row">
-                          <div className="col"></div>
-                          <div className="col-auto">
-                            <div style={style.fontUsia} className="mt-2">
-                              KIRIM
+                    <div className= "ml-2" style={style.kotakLain}>
+                      <div className="container">
+                        <div className="col-md-12">
+                          <div className="row">
+                            <div className="col"></div>
+                            <div className="col-auto">
+                              <div style={style.fontUsia} className="mt-2">
+                                KIRIM
+                              </div>
                             </div>
+                            <div className="col"></div>
                           </div>
-                          <div className="col"></div>
                         </div>
                       </div>
                     </div>
@@ -185,7 +186,6 @@ const style = {
     width: "300.79px",
     height: "64.02px",
     marginLeft: "64px",
-    marginTop: "30px",
     boxShadow: " 6px 6px 0px rgba(0, 0, 0, 0.15)"
   },
 
